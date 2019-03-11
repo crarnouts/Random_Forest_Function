a97ac49f31ccb366a15dbc65e0d13e191df5ffe0
crarnouts <41031719+crarnouts@users.noreply.github.com>
2019-03-11 10:22:52 -0400

    Add files via upload

---
+++ Subset_Columns_Function_piece_of_the_random_forest_function.R
@@ -0,0 +1,17 @@
+subset_columns <- function(data,targetvariable,cols_in_rf){  #target variable is inputted as column vector
+  
+  newdata <- as.data.frame(targetvariable)  ## we intialize the new dataset that this iteration of the random forest will run on (decision tree)
+  
+  ncols <- ncol(data)
+  
+  for (i in 1:cols_in_rf){
+    
+    random_col_num <- round(runif(1, min=1, max=ncols))
+    
+    newdata[[colnames(data[random_col_num])]] <- data[,c(colnames(data[random_col_num]))] # this grabs the random column vector and assigns it
+    # to the new subsetted dataset
+    
+  }
+  
+  return(newdata)
+}
